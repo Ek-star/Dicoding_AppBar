c2cfc8499aa769fecee56852eb886bb86f0b4675
Ekalma Toto Alloy Sembiring <totoekalma@gmail.com>
2024-01-01 14:31:47 +0700

    Last Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -44,4 +48,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
 }
--- app/src/main/java/com/example/myappbar/MenuActivity.kt
+++ app/src/main/java/com/example/myappbar/MenuActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myappbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myappbar.databinding.ActivityMainBinding
+import com.example.myappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
     }
 }
